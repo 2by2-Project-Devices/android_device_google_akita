bfa4cc7803cd6345762e2d44aab2da30ed1ddff0
lahaina <nattolecats@gmail.com>
2024-11-01 20:22:42 +0900

    akita: Set some build flags
    Change-Id: Ib07c8afed0306d251f2945bd12e11291fc7d3e7e

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+
 DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE += $(DEVICE_PATH)/aam_compatibility_matrix.xml
 
 # Kernel modules
